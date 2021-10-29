9aaec9e9b2822c45c1831870855d5a4d0a75a904
tabliang <tabliang@tencent.com>
2021-10-29 11:02:28 +0800

    fix: 完善组件入口引用配置

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const getGitPath = require('./git/getGitPath');
+const formatWebpackMessages = require('./react/formatWebpackMessages');
 
 exports.git = {
   getGitPath,
+};
+
+exports.react = {
+  formatWebpackMessages,
 };
